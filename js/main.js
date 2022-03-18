d73707d4f970d548fb640ea4d3a0dd30ebe51b8f
Aimarest <amarcosestevez@gmail.com>
2022-03-18 11:12:14 +0100

    variables

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const section = document.querySelector(".js-new-form");
+section.classList.remove("collapsed");
+let sectionList = document.querySelector(".js-list");
+let card = document.querySelector(".card");
+let card2 = document.querySelector(".card2");
+let card3 = document.querySelector(".card3");
+
